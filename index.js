1ca1efb3e79b85ef9fc4ad77cf7b9b02edd62e7c
Bonface Masira <bonfacemasira@gmail.com>
2022-06-20 11:08:58 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
 /* Your Code Here */
+const createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+const createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+const createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+const createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+const hoursWorkedOnDate = function(dateSort){
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === dateSort
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === dateSort
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+const wagesEarnedOnDate = function(dateSort){
+    let rawWage = hoursWorkedOnDate.call(this, dateSort)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +76,14 @@ const allWagesFor = function () {
     return payable
 }
 
+const findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(record){
+      return record.firstName === firstName
+    })
+  }
+
+const calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
